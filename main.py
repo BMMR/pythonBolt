af3f429e7d733d59d59ec8aefd830fb90db3452d
Bruno Rocha <60785355+BMMR@users.noreply.github.com>
2023-01-10 19:27:50 +0000

    Loop fixed taking in cosideration the number of id in excel

--- main.py
+++ main.py
@@ -12,38 +12,49 @@ from read_excel import *
 def start_program():
     ###open SC###
     #1º Select, "tag_enter_id"; 2º Write, "tag_write_id"; 3º Select, "tag_open_sc"; 4º) Select, "tag_return";
-    # code to be executed in thread
-    target = ['image/targets/tag_enter_id.png', 'image/targets/tag_write_id.png', 'image/targets/tag_open_sc.png', 'image/targets/tag_return.png']
+    file_to_read="external_info/Battery.xlsx"
+    sheet_name="Baterias"
+    col_name="id_scooter"
 
-    for select_target in target: ## Following rules
-        # Screenshot of screen
-        screen_shot() # Take screen shot of the screen
-        # Location of target
-        #original = 'image/simulation/sim_11.png'
-        original = 'image/screenshot.png'
-        print("Selected_target:" + select_target)
-        time.sleep(1)
+    size_col=return_size_col(file_to_read, sheet_name, col_name)
+    cont=0
 
-        # Start read process
-        center_pointX, center_pointY=start_read_process(original, select_target)
-        # Click on target
-        if center_pointX!="0" and center_pointY!="0":
-            print("click_made")
-            click_touch(center_pointX,center_pointY)
+    while(size_col>cont):
+        cont=cont+1
+
+        # code to be executed in thread
+        target = ['image/targets/tag_enter_id.png', 'image/targets/tag_write_id.png', 'image/targets/tag_open_sc.png', 'image/targets/tag_return.png']
+
+        for select_target in target: ## Following rules
+            # Screenshot of screen
+            screen_shot() # Take screen shot of the screen
+            # Location of target
+            #original = 'image/simulation/sim_11.png'
+            original = 'image/screenshot.png'
+            print("Selected_target:" + select_target)
             time.sleep(1)
-        # Only delect file when the ID was writed
-        if select_target=="image/targets/tag_write_id.png":
-            last_row_id=read_excel('external_info/Battery.xlsx')
-            insert_text(last_row_id) # insert text
-            #delect_last_row('external_info/Battery.xlsx') # delect last row from file
-        else:
-            print("current target:" + select_target)
 
-        print("esperar->" + str(select_target))
-        time.sleep(5)
+            # Start read process
+            center_pointX, center_pointY=start_read_process(original, select_target)
+            # Click on target
+            if center_pointX!="0" and center_pointY!="0":
+                print("click_made")
+                click_touch(center_pointX,center_pointY)
+                time.sleep(0.5)
+            # Only delect file when the ID was writed
+            if select_target=="image/targets/tag_write_id.png":
+                last_row_id=read_excel('external_info/Battery.xlsx')
+                last_row_id="851640"
+                insert_text(last_row_id) # insert text
+                #delect_last_row('external_info/Battery.xlsx') # delect last row from file
+            else:
+                print("current target:" + select_target)
 
-    ###Open SC & take out Battery ###
-    # 1º Select, "tag_enter_id": 2º Write, "tag_write_id": 3º Select, "tag_open_sc", 4º Select, "tag_3_dots"; 5º Select, "tag_replace_batt"; 6º) Select, "tag_return".
+            print("esperar->" + str(select_target))
+
+
+        ###Open SC & take out Battery ###
+        # 1º Select, "tag_enter_id": 2º Write, "tag_write_id": 3º Select, "tag_open_sc", 4º Select, "tag_3_dots"; 5º Select, "tag_replace_batt"; 6º) Select, "tag_return".
 
 
 
@@ -66,8 +77,11 @@ def starting_threads(server):
 
 ################# Main Menu
 if __name__ == '__main__':
-    device = '19173cd4'  # Xiaomi phone (Bruno device)
+    #device = '19173cd4'  # Xiaomi phone (Bruno device)
+    device = 'RFCCT10CRTL'  # SAMSUNG N6 (Bruno device)
+
     variable=0
+
     server=False
     # Create system with order
 
--- read_excel.py
+++ read_excel.py
@@ -1,6 +1,20 @@
 # Load the Excel file
 import pandas as pd
 
+def return_size_col(file_to_read,sheet_name,col_name):
+    # Read the Excel file
+    df = pd.read_excel(file_to_read, sheet_name=sheet_name)
+
+    # Get the size of a specific column
+    column_name = col_name
+    size = df[column_name].size
+
+    print("Size of column '" + column_name + "':", size)
+
+    return size
+
+
+
 def read_excel(file):
 
     df = pd.read_excel(file)
