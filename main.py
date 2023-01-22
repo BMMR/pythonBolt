992adbcf629ca06815f1436783c39f5574b955bb
Bruno Rocha <60785355+BMMR@users.noreply.github.com>
2023-01-22 08:20:18 +0000

    Print values from google sheets

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Obejtive: Send ordens to Bolt Charger in order to send several comands at same time
 # ------> Click_made <------- means ation was taken
 #################################################
+from __future__ import print_function
+
 import io
 import json
 import threading
@@ -13,6 +15,17 @@ from server import *
 from control_android import *
 from image_read import *
 from read_excel import *
+from Google import *
+#############################33
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 
 
 # Funtions related to viewer software
@@ -24,7 +37,6 @@ def start_program(select_mode,thresold,fast_speed,slow_speed,file_to_read,sheet_
 
     while (True): # Main loop, in order to keep the readings of cells
         index=0
-
         for col in colls:
             delect_last_row_val = True  # Delect last ROW By default
 
@@ -36,8 +48,7 @@ def start_program(select_mode,thresold,fast_speed,slow_speed,file_to_read,sheet_
                 center_pointX, center_pointY, max_val=start_read_process('image/screenshot.png', select_target,thresold)
                 # Means that Found the dismiss warning and fail the deploy Means as well that dont need to delect the espefic Row
                 if max_val>thresold and select_target=="image/targets/tag_dismiss_deploy.png":
-                    print("Falhou a leitura, não apaga ID")
-                    delect_last_row_val= True # Fail, do not delect last row
+                    delect_last_row_val= False # Fail, do not delect last row
                 # Click on target
                 if center_pointX!="0" and center_pointY!="0":
                     click_touch(center_pointX,center_pointY)
@@ -48,16 +59,12 @@ def start_program(select_mode,thresold,fast_speed,slow_speed,file_to_read,sheet_
                     # Selected col to insert text
                     col = col.replace("-", "")
                     print("QR SELECTED --->" + col + "<----")
-                    col="851640"
-
                     insert_text(col) # insert text
-
                 else:
                     print("current target:" + select_target)
 
             if delect_last_row_val: # Delect the last row, means that deploy was sucess
-                print("-> delect_selected_row_in_file <--")
-                print("cols"+ str(col) +"index"+ str(index) + " file_read" + str(file_to_read))
+                print(" DELETE QR --> " + str(col) + " with index --> " + str(index))
                 delect_selected_row_in_file(file_to_read,index)
 
         index = index + 1 # Index to delete file
@@ -78,6 +85,59 @@ def starting_threads(server,select_mode,thresold,fast_speed,slow_speed,file_to_r
         thread_1.start()
     # start thread 2
     thread_2.start()
+
+
+def test_drive_connection():
+
+    # If modifying these scopes, delete the file token.json.
+    SCOPES = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    # The ID and range of a sample spreadsheet.
+    SAMPLE_SPREADSHEET_ID = '1P0iPFgKK4M_kiqTUgAHTa4X9NlGWmeZf5DKDPMvweI8'
+    SAMPLE_RANGE_NAME = 'orders'
+
+
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials/client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+    except HttpError as err:
+        print(err)
 
 
 # ----->  Main Menu <----- #
@@ -103,6 +163,8 @@ if __name__ == '__main__':
     file_to_read = "external_info/escooter_orders.xlsx" # File source
     sheet_name="orders" # selected Sheet name
     col_name = "qr_to_command" # selected Column
+    ###########################################
+    test_drive_connection()
 
 
     ###########################################
